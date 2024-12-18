9164f6b66800cf81b4efa59da73a3d536a3caf0f
Tom Veltmeijer <tomveltmeijer@gmail.com>
2024-12-18 08:08:06 +0100

    Working solution for day 12 but ugly code

--- 2024/Code/Day12.cs
+++ 2024/Code/Day12.cs
@@ -2,6 +2,7 @@
 
 using Coordinate = (int x, int y);
 using Direction = (int x, int y);
+using CellFenceStatus = (bool top, bool right, bool bottom, bool left);
 
 public static class Day12
 {
@@ -13,16 +14,6 @@ public static class Day12
     }
 
     public static int CalculateFencePrice(char[][] map)
-    {
-        return CalculateFencePrice(map, hasDiscount: false);
-    }
-
-    public static int CalculateFencePriceWithDiscount(char[][] map)
-    {
-        return CalculateFencePrice(map, hasDiscount: true);
-    }
-
-    private static int CalculateFencePrice(char[][] map, bool hasDiscount)
     {
         List<Coordinate> visited = [];
         int total = 0;
@@ -36,14 +27,166 @@ public static class Day12
                     continue;
                 }
 
-                total += CalculateFencePriceOfRegion(map, visited, startCoordinate, hasDiscount);
+                total += CalculateFencePriceOfRegion(map, visited, startCoordinate);
+            }
+        }
+        return total;
+    }
+
+    public static int CalculateFencePriceWithDiscount(char[][] map)
+    {
+        List<Coordinate> visited = [];
+        var fenceStatuses = new CellFenceStatus[map[0].Length, map.Length];
+        int total = 0;
+        for (int y = 0; y < map.Length; y++)
+        {
+            for (int x = 0; x < map[0].Length; x++)
+            {
+                var startCoordinate = (x, y);
+                if (visited.Contains(startCoordinate))
+                {
+                    continue;
+                }
+
+                total += CalculateFencePriceOfRegionWithDiscount(map, fenceStatuses, visited, startCoordinate);
             }
         }
         return total;
     }
 
     // Calculate price (area * perimeter) of the region that contains the given startCoordinate
-    private static int CalculateFencePriceOfRegion(char[][] map, List<Coordinate> visited, Coordinate startCoordinate, bool hasDiscount)
+    private static int CalculateFencePriceOfRegionWithDiscount(char[][] map, CellFenceStatus[,] fenceStatuses, List<Coordinate> visited, Coordinate startCoordinate)
+    {
+        var queue = new Queue<Coordinate>();
+
+        queue.Enqueue(startCoordinate);
+
+        int area = 0;
+        int minX = startCoordinate.x;
+        int minY = startCoordinate.y;
+        int maxX = startCoordinate.x;
+        int maxY = startCoordinate.y;
+        while (queue.Count > 0)
+        {
+            var cell = queue.Dequeue();
+
+            if (visited.Contains(cell))
+            {
+                continue;
+            }
+            visited.Add(cell);
+
+            ++area;
+            minX = Math.Min(minX, cell.x);
+            maxX = Math.Max(maxX, cell.x);
+            minY = Math.Min(minY, cell.y);
+            maxY = Math.Max(maxY, cell.y);
+
+            Direction[] directions =
+            [
+                (-1,  0),
+                ( 1,  0),
+                ( 0, -1),
+                ( 0,  1),
+            ];
+
+            foreach (var direction in directions)
+            {
+                if (HasFence(map, cell, direction))
+                {
+                    if (direction.x == -1)
+                    {
+                        fenceStatuses[cell.x, cell.y].left = true;
+                    }
+                    else if (direction.x == 1)
+                    {
+                        fenceStatuses[cell.x, cell.y].right = true;
+                    }
+                    else if (direction.y == -1)
+                    {
+                        fenceStatuses[cell.x, cell.y].top = true;
+                    }
+                    else if (direction.y == 1)
+                    {
+                        fenceStatuses[cell.x, cell.y].bottom = true;
+                    }
+                }
+                else
+                {
+                    var neighbor = GetNeighbor(cell, direction);
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        int numSides = 0;
+        for (int x = minX; x <= maxX; x++)
+        {
+            bool prevLeft = false;
+            bool prevRight = false;
+            for (int y = minY; y <= maxY; y++)
+            {
+                var f = fenceStatuses[x, y];
+                if (f.left && !prevLeft)
+                {
+                    ++numSides;
+                }
+                if (f.right && !prevRight)
+                {
+                    ++numSides;
+                }
+                prevLeft = f.left;
+                prevRight = f.right;
+            }
+        }
+
+        for (int y = minY; y <= maxY; y++)
+        {
+            bool prevTop = false;
+            bool prevBottom = false;
+            for (int x = minX; x <= maxX; x++)
+            {
+                var f = fenceStatuses[x, y];
+                if (f.top && !prevTop)
+                {
+                    ++numSides;
+                }
+                if (f.bottom && !prevBottom)
+                {
+                    ++numSides;
+                }
+                prevTop = f.top;
+                prevBottom = f.bottom;
+            }
+        }
+
+        for (int y = minY; y <= maxY; y++)
+        {
+            var debug = "";
+            for (int x = minX; x <= maxX; x++)
+            {
+                var s = fenceStatuses[x, y];
+                debug += "[";
+                debug += s.top ? "T" : " ";
+                debug += s.right ? "R" : " ";
+                debug += s.bottom ? "B" : " ";
+                debug += s.left ? "L" : " ";
+                debug += "]";
+
+                fenceStatuses[x, y].top = false;
+                fenceStatuses[x, y].right = false;
+                fenceStatuses[x, y].bottom = false;
+                fenceStatuses[x, y].left = false;
+            }
+            Console.WriteLine(debug);
+        }
+        Console.WriteLine("");
+
+        return area * numSides;
+    }
+
+    // Calculate price (area * perimeter) of the region that contains the given startCoordinate
+    private static int CalculateFencePriceOfRegion(char[][] map, List<Coordinate> visited, Coordinate startCoordinate)
     {
         var queue = new Queue<Coordinate>();
 
@@ -75,11 +218,6 @@ public static class Day12
             {
                 if (HasFence(map, cell, direction))
                 {
-                    if (hasDiscount && IsFenceExtensionOfVisitedFence(map, cell, direction, visited))
-                    {
-                        continue;
-                    }
-
                     ++fenceCount;
                 }
                 else
@@ -111,27 +249,6 @@ public static class Day12
 
     private static bool AreInSameRegion(char[][] map, Coordinate a, Coordinate b)
     {
-        return map[a.x][a.y] == map[b.x][b.y];
-    }
-    private static bool IsFenceExtensionOfVisitedFence(char[][] map, Coordinate coordinate, Direction direction, IList<Coordinate> visited)
-    {
-        if (direction.x == 0)
-        {
-            var left = GetNeighbor(coordinate, (-1, 0));
-            var right = GetNeighbor(coordinate, (1, 0));
-            return HasExistingFenceInSameDirection(left) || HasExistingFenceInSameDirection(right);
-        }
-        else
-        {
-            var up = GetNeighbor(coordinate, (0, -1));
-            var down = GetNeighbor(coordinate, (0, 1));
-            return HasExistingFenceInSameDirection(up) || HasExistingFenceInSameDirection(down);
-        }
-
-        bool HasExistingFenceInSameDirection(Coordinate neighbor) =>
-            IsInBounds(map, neighbor)
-            && AreInSameRegion(map, coordinate, neighbor)
-            && visited.Contains(neighbor)
-            && HasFence(map, neighbor, direction);
+        return map[a.y][a.x] == map[b.y][b.x];
     }
 }
